f528c2dfae5e9e71f8c670116ed283ba2a232bb0
Saral Bajracharya <rmanbajra@gmail.com>
2023-07-16 18:45:22 +0545

    using arrow function

--- main.js
+++ main.js
@@ -12,5 +12,32 @@ const companies= [
   
   const ages = [33, 12, 20, 16, 5, 54, 21, 44, 61, 13, 15, 45, 25, 64, 32];
 
+
+    // for(let i = 0; i<companies.length; i++){
+    //     console.log(companies[i]);
+    // }
   //forEach
-  
+    // companies.forEach(function(company) {
+    //     console.log(company.name)
+
+    // })
+
+    //filter
+
+//   let canDrink = [];
+//   for(let i = 0; i < ages.length; i++){
+//     if(ages[i] >= 21) {
+//         canDrink.push(ages[i]);
+//     }
+//   }
+
+
+//   const canDrink = ages.filter(function(age) {
+//     if(age >= 21) {
+//         return true;
+//     }
+//   })
+
+  //using arrow function
+  const canDrink = ages.filter(age => age >= 21 );
+  console.log(canDrink)
